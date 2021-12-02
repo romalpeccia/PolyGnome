bc419d1e5af48e78b7661f325b2e6a06a5a4ff8e
romalpeccia <romal.peccia@gmail.com>
2021-12-02 11:04:40 -0500

    added subdivision sample loading and playing logic (WIP)

--- Source/Metronome.cpp
+++ Source/Metronome.cpp
@@ -18,16 +18,33 @@ Metronome::Metronome()
     //load up click file
     //TODO multiple files
         formatManager.registerBasicFormats();
-        juce::String dir;
+        /*
+        juce::String workingDir, fileDir;
+        workingDir = juce::File::getCurrentWorkingDirectory().getFullPathName();
+        
+        fileDir = workingDir + "/Samples/rimshot.wav"; 
+        juce::File clickFile(fileDir);
+        */
 
         //TODO don't hardcode this filepath
-        juce::File clickFile("C:/Users/Romal/Desktop/JUCE_PROJECTS/MetroGnome/Samples/rimshot.wav");
+        //TODO write file loader
+        juce::File clickFile("C:/Users/Romal/Desktop/JUCE_PROJECTS/MetroGnome/Samples/rimshot_low.wav");
         if (!clickFile.exists())
         {
             jassert("file not found");
         }
         auto formatReader = formatManager.createReaderFor(clickFile);
-        pMetronomeSample.reset(new juce::AudioFormatReaderSource(formatReader, true));
+        rimShotLow.reset(new juce::AudioFormatReaderSource(formatReader, true));
+
+        /*
+        juce::File clickFile2("C:/Users/Romal/Desktop/JUCE_PROJECTS/MetroGnome/Samples/rimshot_high.wav");
+        if (!clickFile2.exists())
+        {
+            jassert("file not found");
+        }
+        auto formatReader2 = formatManager.createReaderFor(clickFile2);
+        rimShotHigh.reset(new juce::AudioFormatReaderSource(formatReader2, true));
+        */
     //end load up click file
 }
 
@@ -37,12 +54,16 @@ void Metronome::prepareToPlay(double _sampleRate, int samplesPerBlock)
 {
     sampleRate = _sampleRate;
     beatInterval = (60.0 / bpm) * sampleRate;
-    
+    subInterval = (60 / bpm) * beatInterval;
 
-    if (pMetronomeSample != nullptr)
+    if (rimShotLow != nullptr)
     {
-        pMetronomeSample->prepareToPlay(samplesPerBlock, sampleRate);
+        rimShotLow->prepareToPlay(samplesPerBlock, sampleRate);
        // DBG("file loaded");
+    }
+    if (rimShotHigh != nullptr)
+    {
+        rimShotHigh->prepareToPlay(samplesPerBlock, sampleRate);
     }
 
 
@@ -52,35 +73,59 @@ void Metronome::getNextAudioBlock(juce::AudioBuffer<float>& buffer)
 {
     //TODO fix this entire function, seems hacky 
     
+    //temp because <juce::AudioFormatReaderSource>->getNextAudioBlock expects an AudioSourceChannelInfoObject
     auto temp = juce::AudioSourceChannelInfo(buffer);
-    //temp.buffer->makeCopyOf(buffer);
-
 
     auto bufferSize = buffer.getNumSamples();
 
+
+
     totalSamples += bufferSize;
     samplesProcessed = totalSamples % beatInterval;
-
 
     if (samplesProcessed + bufferSize >= beatInterval)
 
     {
         const auto timeToStartPlaying = beatInterval - samplesProcessed;
-        pMetronomeSample->setNextReadPosition(0); //reset sample to beginning
+        rimShotLow->setNextReadPosition(0); //reset sample to beginning
 
         for (auto samplenum = 0; samplenum < bufferSize + 1; samplenum++)
         {
             if (samplenum == timeToStartPlaying)
             {
-                pMetronomeSample->getNextAudioBlock(temp);
+                rimShotLow->getNextAudioBlock(temp);
             }
         }
     }
-    if (pMetronomeSample->getNextReadPosition() != 0)           //if we already started playing and got interupted
+    else if (samplesProcessed + bufferSize >= subInterval)
     {
-        pMetronomeSample->getNextAudioBlock(temp);
+        // make this if statement first?
+        // make this a function?
+        const auto timeToStartPlaying = beatInterval - samplesProcessed;
+        rimShotHigh->setNextReadPosition(0); //reset sample to beginning
+
+        for (auto samplenum = 0; samplenum < bufferSize + 1; samplenum++)
+        {
+            if (samplenum == timeToStartPlaying)
+            {
+                rimShotHigh->getNextAudioBlock(temp);
+            }
+        }
+
+    }
+
+ //if we already started playing and got interupted
+    if (rimShotLow->getNextReadPosition() != 0)          
+    {
+        rimShotLow->getNextAudioBlock(temp);
+    }
+    else if (rimShotHigh->getNextReadPosition() != 0)
+    {
+        rimShotHigh->getNextAudioBlock(temp);
     }
 }
+
+
 
 void Metronome::reset() 
 {
--- Source/Metronome.h
+++ Source/Metronome.h
@@ -24,12 +24,19 @@ class Metronome
 
 
     private:
-        int totalSamples = 0; //total samples since start time
+        int totalSamples = 0; //total samples since start time, relevant?
         double sampleRate = 0; 
         int beatInterval = 0; //interval representing one beat click = (60.0 / bpm) * sampleRate
+        int subInterval = 0;
+        int subAmount = 4; // TODO make this value a param
         double bpm = 240; 
         int samplesProcessed = 0; // samples processed before beat = totalSamples % interval
 
         juce::AudioFormatManager formatManager;
-        std::unique_ptr <juce::AudioFormatReaderSource> pMetronomeSample =  nullptr ;
+
+        //start code for loading multiple files
+        //static const int numWAVs = 2;
+        //std::array <std::unique_ptr <juce::AudioFormatReaderSource>, numWAVs> pMetronomeSamples;
+        std::unique_ptr <juce::AudioFormatReaderSource> rimShotLow;
+        std::unique_ptr <juce::AudioFormatReaderSource> rimShotHigh;
 };
